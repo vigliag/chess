cdf1bf1f830700e1be775548c75cfcf866cf144d
Gabriele <gabry.501@live.it>
2014-08-01 19:32:18 +0200

    initial express app

--- public/main.js
+++ public/main.js
@@ -1,237 +1,234 @@
-
-   var sessione;
-   var dati = new Array();
-   var socket = io.connect("http://localhost:3000/",{'connect timeout': 400});
-   $('#chat').on('submit', function() {
-    dati[0]=$('#user').val();
-    dati[1]=$('#m').val();
-    socket.emit('chatmessage', dati,sessione);
+var sessione;
+var dati = new Array();
+var socket = io.connect("http://localhost:3000/", {'connect timeout': 400});
+$('#chat').on('submit', function () {
+    dati[0] = $('#user').val();
+    dati[1] = $('#m').val();
+    socket.emit('chatmessage', dati, sessione);
     $('#m').val('');
     return false;
-  });
-  var myid=localStorage.getItem('ID');
- var myusers;
- var color;
- var mystato;
- 
-   socket.on('connect', function(){
-       socket.emit("lista");
- 
-  });
+});
+var myid = localStorage.getItem('ID');
+var myusers;
+var color;
+var mystato;
 
- 
-   socket.on('stanze', function(stanzelist){
-    var string="";
-      for(i=0;i<stanzelist.length;i++){
-         string+="<li>"+stanzelist[i]+"</li><button onclick='entra("+stanzelist[i]+")'>Entra</button>";
-        
-      }
-      $("#stanze").html(string).trigger('create');
-    
-  });
-  
- 
-  socket.on('id', function(id){
-  	 if(!myid) {
-       myid=id;
-       localStorage.setItem('ID',id);
-      }else{
-      socket.emit("settaid",myid);	
-      	
-      }
-  });
-  
-   socket.on('stato', function(stato){
-       mystato=stato;
-  });
-  
-  
-  socket.on('chatmessage', function(msg){
-    $('#messages').append('<li><h3>'+msg[0]+':</h3>'+msg[1]+'</li>');
-  });
-  
-   socket.on('room', function(room){
+socket.on('connect', function () {
     socket.emit("lista");
-  });
-  
-  
-  socket.on('fen', function(fen){
-     game.load(fen);
-     board.position(game.fen());
-     if((game.turn()=="w" && color=="black")||(game.turn()=="b" && color=="white")){
-     cfg.draggable=false;
-   }else{
-    
-    cfg.draggable=true;
-   }
-  });
-  
-   socket.on('users', function(users,room){
-   console.log("starts");
-   myusers=users;
-    if(users["ID1"]==myid){
-       board.orientation('white');
-       color="white";
-    socket.emit("refresh",room);
 
-    }else{
-       board.orientation('black');
-       color="black";
+});
+
+
+socket.on('stanze', function (stanzelist) {
+    var string = "";
+    for (i = 0; i < stanzelist.length; i++) {
+        string += "<li>" + stanzelist[i] + "</li><button onclick='entra(" + stanzelist[i] + ")'>Entra</button>";
+
     }
-    
-   
-  });
-  
+    $("#stanze").html(string).trigger('create');
+
+});
 
 
-function crea(){
+socket.on('id', function (id) {
+    if (!myid) {
+        myid = id;
+        localStorage.setItem('ID', id);
+    } else {
+        socket.emit("settaid", myid);
 
- socket.emit("create");   
- document.getElementById("creazione").style.display="none"; 
+    }
+});
+
+socket.on('stato', function (stato) {
+    mystato = stato;
+});
+
+
+socket.on('chatmessage', function (msg) {
+    $('#messages').append('<li><h3>' + msg[0] + ':</h3>' + msg[1] + '</li>');
+});
+
+socket.on('room', function (room) {
+    socket.emit("lista");
+});
+
+
+socket.on('fen', function (fen) {
+    game.load(fen);
+    board.position(game.fen());
+    if ((game.turn() == "w" && color == "black") || (game.turn() == "b" && color == "white")) {
+        cfg.draggable = false;
+    } else {
+
+        cfg.draggable = true;
+    }
+});
+
+socket.on('users', function (users, room) {
+    console.log("starts");
+    myusers = users;
+    if (users["ID1"] == myid) {
+        board.orientation('white');
+        color = "white";
+        socket.emit("refresh", room);
+
+    } else {
+        board.orientation('black');
+        color = "black";
+    }
+
+
+});
+
+
+function crea() {
+
+    socket.emit("create");
+    document.getElementById("creazione").style.display = "none";
 }
 
-function entra(room){
-socket.emit("subscribe",room);  
-wait(room);
+function entra(room) {
+    socket.emit("subscribe", room);
+    wait(room);
 }
 
 
-function join(){
-room=document.getElementById("entra").value;
-socket.emit("subscribe",room);  
-wait(room);
+function join() {
+    room = document.getElementById("entra").value;
+    socket.emit("subscribe", room);
+    wait(room);
 }
 
-function continua(room){
-if(mystato!="errore"){
-sessione=room;
-document.getElementById("gestione").style.display="none"; 
-document.getElementById("board").style.display="block"; 
-document.getElementById("dx").style.display="block";  
+function continua(room) {
+    if (mystato != "errore") {
+        sessione = room;
+        document.getElementById("gestione").style.display = "none";
+        document.getElementById("board").style.display = "block";
+        document.getElementById("dx").style.display = "block";
+    }
 }
-}
 
 
-
-function wait(room){
+function wait(room) {
     setTimeout(
         function () {
             if (mystato) {
-continua(room);
-return;
+                continua(room);
+                return;
             } else {
-                
+
                 wait();
             }
 
-        }, 200); 
+        }, 200);
 }
 var board,
-  game = new Chess(),
-  statusEl = $('#status'),
-  fenEl = $('#fen'),
-  pgnEl = $('#pgn');
+    game = new Chess(),
+    statusEl = $('#status'),
+    fenEl = $('#fen'),
+    pgnEl = $('#pgn');
 
 // do not pick up pieces if the game is over
 // only pick up pieces for the side to move
-var onDragStart = function(source, piece, position, orientation) {
-  if (game.game_over() === true ||
-      (game.turn() === 'w' && piece.search(/^b/) !== -1) ||
-      (game.turn() === 'b' && piece.search(/^w/) !== -1)) {
-    return false;
-  }
+var onDragStart = function (source, piece, position, orientation) {
+    if (game.game_over() === true ||
+        (game.turn() === 'w' && piece.search(/^b/) !== -1) ||
+        (game.turn() === 'b' && piece.search(/^w/) !== -1)) {
+        return false;
+    }
 };
 
-var onDrop = function(source, target) {
-  // see if the move is legal
-  var move = game.move({
-    from: source,
-    to: target,
-    promotion: 'q' // NOTE: always promote to a queen for example simplicity
-  });
+var onDrop = function (source, target) {
+    // see if the move is legal
+    var move = game.move({
+        from: source,
+        to: target,
+        promotion: 'q' // NOTE: always promote to a queen for example simplicity
+    });
 
-  // illegal move
-  if (move === null) return 'snapback';
+    // illegal move
+    if (move === null) return 'snapback';
 
-  updateStatus();
+    updateStatus();
 };
 
 // update the board position after the piece snap 
 // for castling, en passant, pawn promotion
-var onSnapEnd = function() {
-	
-  if((game.turn()=="w" && color=="black")||(game.turn()=="b" && color=="white")){
-     cfg.draggable=false;
-   }else{
-   	
-   	cfg.draggable=true;
-   }	
-	
-  board.position(game.fen());
+var onSnapEnd = function () {
+
+    if ((game.turn() == "w" && color == "black") || (game.turn() == "b" && color == "white")) {
+        cfg.draggable = false;
+    } else {
+
+        cfg.draggable = true;
+    }
+
+    board.position(game.fen());
 };
 
-var updateStatus = function() {
-  var status = '';
+var updateStatus = function () {
+    var status = '';
 
-  var moveColor = 'White';
-  if (game.turn() === 'b') {
-    moveColor = 'Black';
-  }
-
-  // checkmate?
-  if (game.in_checkmate() === true) {
-    status = 'Game over, ' + moveColor + ' is in checkmate.';
-  }
-
-  // draw?
-  else if (game.in_draw() === true) {
-    status = 'Game over, drawn position';
-  }
-
-  // game still on
-  else {
-    status = moveColor + ' to move';
-
-    // check?
-    if (game.in_check() === true) {
-      status += ', ' + moveColor + ' is in check';
+    var moveColor = 'White';
+    if (game.turn() === 'b') {
+        moveColor = 'Black';
     }
-  }
-if(socket){
-socket.emit('fen', game.fen()); 
- }
-  
+
+    // checkmate?
+    if (game.in_checkmate() === true) {
+        status = 'Game over, ' + moveColor + ' is in checkmate.';
+    }
+
+    // draw?
+    else if (game.in_draw() === true) {
+        status = 'Game over, drawn position';
+    }
+
+    // game still on
+    else {
+        status = moveColor + ' to move';
+
+        // check?
+        if (game.in_check() === true) {
+            status += ', ' + moveColor + ' is in check';
+        }
+    }
+    if (socket) {
+        socket.emit('fen', game.fen());
+    }
+
 };
 
 var cfg = {
-  draggable: true,
-  position: 'start',
-  onDragStart: onDragStart,
-  onDrop: onDrop,
-  onSnapEnd: onSnapEnd
+    draggable: true,
+    position: 'start',
+    onDragStart: onDragStart,
+    onDrop: onDrop,
+    onSnapEnd: onSnapEnd
 };
 board = new ChessBoard('board', cfg);
 
 updateStatus();
 
 
-function joina(){
-IDsess=document.getElementById("joina").val();
-window.location.href="/"+IDsess;	
-	
+function joina() {
+    IDsess = document.getElementById("joina").val();
+    window.location.href = "/" + IDsess;
+
 }
 
-$( document ).ready(function() {
-var par = getUrlVars()["ID"];
-if (par){
-entra(par);
-}
+$(document).ready(function () {
+    var par = getUrlVars()["ID"];
+    if (par) {
+        entra(par);
+    }
 });
 
 
 function getUrlVars() {
     var vars = {};
-    var parts = window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi, function(m,key,value) {
+    var parts = window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi, function (m, key, value) {
         vars[key] = value;
     });
     return vars;
--- server.js
+++ server.js
@@ -1,107 +1,110 @@
 var express = require('express');
 var http = require('http');
 var app = express();
+var dustjs = require('adaro');
+
+app.engine('dust', dustjs.dust({cache: false}));
+app.set('view engine', 'dust');
+
 app.use(express.static(__dirname + '/public'));
+
+app.get('/chess/:id', function (req, res) {
+    var id = req.param("id");
+    var room = rooms[id];
+    res.render('room', {roomid: id, room: room, white: true});
+})
+
+
 var server = http.createServer(app).listen(3000);
+console.log("listening on port 3000");
 var io = require('socket.io')(server);
 
-io.on('connection', function(socket){
-	
-  socket.on('chatmessage', function(msg,room){
-     socket.broadcast.to(room).emit('chatmessage', msg);
-  });
-  
-   socket.on('fen', function(msg){
-     io.emit('fen', msg);
-  });
+io.on('connection', function (socket) {
+
+    socket.on('chatmessage', function (msg, room) {
+        socket.broadcast.to(room).emit('chatmessage', msg);
+    });
+
+    socket.on('fen', function (msg) {
+        io.emit('fen', msg);
+    });
 });
 
 // Room class
-function Room(){
-  this.users = {
-      ID1: null,
-      ID2: null
-  } 
+function Room() {
+    this.users = {
+        white: null, //{cookie:null, socketId: null},
+        black: null
+    }
 }
 
-var nick= new Array();
+
+var nick = new Array();
 
 
-Room.prototype.isFull = function() {
-  return this.users.ID2 !== null;
-  
+Room.prototype.isFull = function () {
+    return this.users.ID2 !== null || this.users.ID1 !== null;
 };
 
-Room.prototype.join = function(socketId) {
-  if(this.users.ID1 === null)
-    return this.users.ID1 = nick[socketId]; //è bianco 
-  if(this.users.ID2 === null)
-    return this.users.ID2 = nick[socketId];
-  throw new Error('room Full');
+Room.prototype.register = function (color, socketId) {
+    if (this.users[color] === null){
+        this.users[color] = {
+            cookie : 123,
+            socketId : socketId
+        }
+        return this.users[color];
+    }
 }
 
 var rooms = {};
 
-function availableRooms(){
-  return Object.keys(rooms).filter(function(roomId){
-      return !rooms[roomId].isFull();
-  })
+function availableRooms() {
+    return Object.keys(rooms).filter(function (roomId) {
+        return !rooms[roomId].isFull();
+    })
 }
 
-io.sockets.on('connection', function(socket){
+io.sockets.on('connection', function (socket) {
 
-nick[socket.id]=socket.id; //assegno un nick che è l'id
+    nick[socket.id] = socket.id; //assegno un nick che è l'id
 
-socket.emit('id', socket.id); //mando l'id ma se è gia esistente il client torna id vecchio
+    socket.emit('id', socket.id); //mando l'id ma se è gia esistente il client torna id vecchio
 
-console.log(socket.id);	
+    console.log(socket.id);
 
-  socket.on('create', function() { 
-	var roomId = Math.floor((Math.random() * 1000) + 1);
-	if(rooms[roomId]) return socket.to(socket.id).emit('room',"errore");
+    socket.on('create', function () {
+        var roomId = Math.floor((Math.random() * 1000) + 1);
+        if (rooms[roomId]) return socket.to(socket.id).emit('room', "errore");
 
-	rooms[roomId] = new Room();
+        rooms[roomId] = new Room();
 
-	console.log(rooms);
-    //socket.join(newroom);
-	socket.emit('room',roomId); 
-	socket.broadcast.emit('stanze', availableRooms());//dico a tutti le stanze
-});
+        console.log(rooms);
+        //socket.join(newroom);
+        socket.emit('room', roomId);
+        socket.broadcast.emit('stanze', availableRooms());//dico a tutti le stanze
+    });
 
-  socket.on('settaid', function(myid) { 
-	nick[socket.id]=myid;
-	console.log(nick[socket.id]+"="+myid);
-});
+    socket.on('settaid', function (myid) {
+        nick[socket.id] = myid;
+        console.log(nick[socket.id] + "=" + myid);
+    });
 
-    
-socket.on('lista',function(){
-    socket.emit('stanze', availableRooms());//dico le stanze a chi le ha chieste
-});
-    
-socket.on('refresh',function(roomId){
-  socket.broadcast.to(roomId).emit('users', rooms[roomId].users, roomId);
-});
-  
-  
-  socket.on('disconnect', function() {
-    //user[id]=null; AGGIUSTA TU
-   }); 
-  
-socket.on('subscribe', function(roomId) { 
-    var room = rooms[roomId]
-	  if (!room){ //se la stanza non esiste
-      socket.to(socket.id).emit("stato", "errore");
-    } else {
-      if(room.isFull()){
-        socket.emit('message', "stanza piena");
-      } else {
-        room.join(socket.id);
+    socket.on('disconnect', function () {
+        //user[id]=null; AGGIUSTA TU
+    });
+
+    socket.on('subscribe', function (roomId, color) {
+        var room = rooms[roomId];
+        if (!room) { //se la stanza non esiste
+            return socket.to(socket.id).emit("stato", "errore");
+        }
+
+        if(rooms.users[color])
+            return socket.to(socket.id).emit("stato", "errore");
+
+        var user = rooms.register(color, socket.id);
         socket.join(roomId);
-        socket.emit("stato", "approvato");
-      }
-    }
-
-   socket.broadcast.emit('stanze',availableRooms());//dico a tutti le stanze
-   socket.broadcast.to(roomId).emit('users', room.users ,roomId);	//comunica array w/b
-});//socket subscribe  
+        socket.emit("approved", {color: color});
+        socket.broadcast.to(roomId).emit('users', {white: room.users.white, black: room.users.black});
+    });//socket subscribe
 });//io connection
